7c8dba7738a90fc88f185c2ae9934482a295e8e9
William Dong <williamdong@Williams-MacBook-Pro.local>
2022-09-11 11:44:01 +1000

    added terraform part

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "sec_gid" {
+  description = "The id for your default security group"
+  type        = string
+}
+
+variable "ec2_count" {
+  description = "The number of EC2 instances to be created"
+  type        = number
+}
